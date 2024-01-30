1d86b5df2aa72ca9efb1ba36369a786369fbb9b2
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2024-01-30 17:30:16 +0400

    delete modal, localstorage, events and dynamic ui set

--- build/js/main.js
+++ build/js/main.js
@@ -1,102 +1,214 @@
 "use strict";
-const addItemInput = document.querySelector("form input");
 const form = document.querySelector("form");
-const shopList = document.querySelector(".items");
-const clearBtn = document.querySelector("#clear");
-// clearBtn.classList.add("disabled");
-/* -------------------------------------------------------------------------- */
-/*                    Add item to top or bottom of the list                   */
-/* -------------------------------------------------------------------------- */
+const addItemInput = form.querySelector("input");
+const shopContainer = document.querySelector(".items");
 const toggleBtn = document.querySelector("#toggle");
-function toggleButton(e) {
-    console.log("saw");
-    const isUp = toggleBtn.textContent === "⬆";
-    toggleBtn.textContent = isUp ? "⬇" : "⬆";
-    toggleBtn.setAttribute("data-direction", isUp ? "down" : "up");
-}
-toggleBtn.addEventListener("click", toggleButton);
+const emptyInfo = shopContainer.querySelector("h4");
+const filterInput = document.querySelector("#filter-input");
+const clearBtn = document.querySelector("#clear");
+const modal = document.querySelector("#modal");
+const list = createElement("ul");
+list.setAttribute("id", "items-list");
+list.className = "p-2 grid gap-2 font-bold";
+/* ------------------ Add item to top or bottom of the list ----------------- */
+const toggleButton = () => toggleBtn.classList.toggle("rotate-180");
 /* -------------------------- Creating New Element -------------------------- */
-function createElement(tag) {
-    return document.createElement(tag);
+function createElement(tag, className) {
+    const element = document.createElement(tag);
+    if (tag === "button")
+        element.setAttribute("type", "button");
+    return element;
 }
-/* -------------------------------------------------------------------------- */
-/*                           Creating new list item                           */
-/* -------------------------------------------------------------------------- */
-function createButtonsOfItem(label) {
-    const remBtn = createElement("button");
-    remBtn.textContent = label;
-    remBtn.addEventListener("click", (e) => {
-        console.log("clicked on remove button");
-    });
-    return remBtn;
-}
+/* ------------------------- Creating new list item ------------------------- */
 function createNewItem(value) {
-    // li item
+    // <li> item
     const newItem = createElement("li");
     newItem.setAttribute("id", "item");
     newItem.className = "p-2 flex items-center justify-between border-2 border-black/30 rounded-md";
-    // span text
+    // <span> text
     const text = createElement("span");
     text.textContent = value;
-    /* --------------------------------- Buttons -------------------------------- */
+    // <div> <buttons> </div>
     const allBtns = createElement("div");
-    allBtns.className = "flex gap-2 bg-gray-300 rounded-md p-1";
+    allBtns.className = "flex gap-2 bg-gray-300 rounded-md p-1 items-center";
     const remBtn = createElement("button");
     remBtn.textContent = "❌";
+    remBtn.setAttribute("id", "remove");
     const editBtn = createElement("button");
     editBtn.textContent = "➕";
-    const doneBtn = createElement("button");
-    doneBtn.textContent = "✔";
-    allBtns.addEventListener("click", (e) => {
-        console.log("clicked on button");
-    });
-    [doneBtn, editBtn, remBtn].forEach((btn) => {
-        btn.setAttribute("type", "button");
+    editBtn.setAttribute("id", "edit");
+    const doneBtn = createElement("input");
+    doneBtn.type = "checkbox";
+    doneBtn.className = "w-5 h-5";
+    allBtns.appendChild(doneBtn);
+    [
+        { id: "edit", el: editBtn },
+        { id: "remove", el: remBtn },
+    ].forEach(({ el: btn, id }) => {
+        btn.setAttribute("id", id);
         allBtns.appendChild(btn);
     });
-    /* --------------------------------- Buttons -------------------------------- */
     newItem.appendChild(text);
     newItem.appendChild(allBtns);
     return newItem;
 }
-let allValues = localStorage.getItem("list") ? JSON.parse(localStorage.getItem("list")) : [];
+/* ------------------------- Get items from storage ------------------------- */
+function getItemsFromStorage() {
+    return JSON.parse(localStorage.getItem("items") || "[]");
+}
+/* ------------------------------- Remove Item ------------------------------- */
+function removeItem(element, text) {
+    element.remove();
+    const filtered = getItemsFromStorage().filter((item) => item !== text);
+    if (list.children.length === 0) {
+        replaceListAndEmpty(emptyInfo);
+    }
+    localStorage.setItem("items", JSON.stringify(filtered));
+    updateActions();
+}
+/* ------------------------------- Done Item ------------------------------- */
+function checkItem(element) {
+    console.log(element);
+    element.classList.toggle("line-through");
+    element.classList.toggle("font-normal");
+    element.classList.toggle("opacity-50");
+}
+function openDialog(element, type, text) {
+    modal.classList.replace("opacity-0", "opacity-100");
+    modal.classList.replace("pointer-events-none", "pointer-events-auto");
+    const dialog = document.querySelector("#back-drop");
+    Array.from(dialog.children).forEach((child) => (child.getAttribute("id") !== type ? child.classList.add("hidden") : null));
+    console.log(dialog.firstElementChild);
+    dialog.addEventListener("click", deleteItem);
+    function deleteItem(e) {
+        const clicked = e.target;
+        const success = clicked.getAttribute("id") === "yes";
+        if (clicked.nodeName === "BUTTON" && text) {
+            if (success) {
+                removeItem(element, text);
+            }
+            closeDialog();
+        }
+    }
+}
+function closeDialog() {
+    modal.classList.replace("opacity-100", "opacity-0");
+    modal.classList.replace("pointer-events-auto", "pointer-events-none");
+    const dialog = document.querySelector("#back-drop");
+    Array.from(dialog.children).forEach((child) => child.classList.add("hidden"));
+}
+function addActionstoBtn(e) {
+    const clickedBtn = e.target;
+    const parent = clickedBtn.closest("li");
+    const text = parent.querySelector("span");
+    switch (clickedBtn.getAttribute("id")) {
+        case "remove":
+            openDialog(parent, "remove", text.textContent);
+            break;
+        case "edit":
+            // removeItem(parent, text.textContent!);
+            openDialog(parent, "edit", text.textContent);
+            break;
+        case "done":
+            checkItem(text);
+            break;
+    }
+}
+/* ------------------------- Replace Project Content ------------------------ */
+const replaceListAndEmpty = (element) => shopContainer.replaceChild(element, shopContainer.firstElementChild);
+/* ------------------------------- Form Submit ------------------------------ */
 function handleSubmit(e) {
-    var _a;
     e.preventDefault();
-    const value = addItemInput.value;
+    const value = addItemInput.value.trim();
     if (!value) {
         return alert("Please enter a value");
     }
-    // Select list or create if there is no list yest
-    let list = (_a = document.querySelector("#items-list")) !== null && _a !== void 0 ? _a : null;
-    if (!list) {
-        list = createElement("ul");
-        list.setAttribute("id", "items-list");
-        list.className = "p-2 grid gap-2 font-bold";
+    else if (getItemsFromStorage().includes(value)) {
+        return alert("Item already exists");
     }
+    else {
+        // Add to UI and storage
+        addItemToUI(value);
+        // Add to  storage
+        addItemToStorage(value);
+        // Clear and focus input
+        addItemInput.value = "";
+        addItemInput.focus();
+        // replaceListAndEmpty
+        updateActions();
+    }
+}
+function addItemToUI(value) {
     const item = createNewItem(value);
     // add to UI
-    const addToUp = toggleBtn.getAttribute("data-direction") === "up" ? true : false;
-    addToUp ? list.insertAdjacentElement("afterbegin", item) : list.appendChild(item);
-    allValues.push(value);
-    shopList.innerHTML = list.outerHTML;
-    // add to localstorage
-    localStorage.setItem("list", JSON.stringify([...allValues, value]));
-    // Clear and focus input
-    addItemInput.value = "";
-    addItemInput.focus();
-    // Change opacity ofclear btn
-    clearBtn.classList.remove("disabled");
-    console.log("created");
+    const addToUp = toggleBtn.classList.contains("rotate-180");
+    if (addToUp) {
+        list.insertAdjacentElement("afterbegin", item);
+    }
+    else {
+        list.appendChild(item);
+    }
+    // Replace empty list with new list
+    replaceListAndEmpty(list);
 }
-form.addEventListener("submit", handleSubmit);
-// Clear All Items
-// let hasItems = shopList.contains(list);
-function clearAll() {
-    const emtpyList = createElement("h4");
-    emtpyList.textContent = "Empty list";
-    emtpyList.className = "text-3xl text-center font-bold my-4 opacity-50";
-    // clearBtn.classList.add("disabled");
-    shopList.innerHTML = emtpyList.outerHTML;
+function addItemToStorage(value) {
+    const items = getItemsFromStorage();
+    items.push(value);
+    localStorage.setItem("items", JSON.stringify(items));
 }
-clearBtn.addEventListener("click", clearAll);
+/* ------------------------------ clearAllItems ----------------------------- */
+const clearAll = () => replaceListAndEmpty(emptyInfo);
+/* ------------------------------ Filter Items ------------------------------ */
+function filterItems(e) {
+    const allItems = list.querySelectorAll("#item");
+    const value = e.target.value.toLowerCase();
+    allItems.forEach((item) => {
+        var _a, _b;
+        const itemContext = (_b = (_a = item.firstChild) === null || _a === void 0 ? void 0 : _a.textContent) === null || _b === void 0 ? void 0 : _b.toLowerCase();
+        if (!itemContext.includes(value)) {
+            item.classList.add("hidden");
+        }
+        else {
+            item.classList.remove("hidden");
+        }
+    });
+    e.target;
+}
+/* ------------------------------ UI Actions ------------------------------ */
+function emptyListUI() {
+    clearBtn.setAttribute("disabled", "true");
+    filterInput.classList.add("hidden");
+}
+function nonEmptyListUI() {
+    console.log("chagirildi");
+    filterInput.classList.remove("hidden");
+    clearBtn.removeAttribute("disabled");
+}
+function updateActions() {
+    const items = list.querySelectorAll("#item");
+    // it must be reacreated every time it's called because we have to know current state of items
+    if (items.length === 0)
+        emptyListUI();
+    else if (items.length === 1)
+        nonEmptyListUI();
+}
+function displayItems() {
+    const storageItems = getItemsFromStorage();
+    if (storageItems.length > 0) {
+        storageItems.forEach((item) => {
+            list.appendChild(createNewItem(item));
+        });
+        replaceListAndEmpty(list);
+    }
+}
+/* ----------------------------- Event Listeners ---------------------------- */
+function initApp() {
+    toggleBtn.addEventListener("click", toggleButton);
+    list.addEventListener("click", addActionstoBtn);
+    form.addEventListener("submit", handleSubmit);
+    clearBtn.addEventListener("click", clearAll);
+    // filterInput.addEventListener("input", filterItems);
+    displayItems();
+    updateActions();
+}
+initApp();
--- src/main.ts
+++ src/main.ts
@@ -1,82 +1,63 @@
-const addItemInput = document.querySelector("form input") as HTMLInputElement;
 const form = document.querySelector("form") as HTMLFormElement;
-const shopList = document.querySelector(".items") as HTMLDivElement;
-const clearBtn = document.querySelector("#clear") as HTMLButtonElement;
-
-// clearBtn.classList.add("disabled");
-
-/* -------------------------------------------------------------------------- */
-/*                    Add item to top or bottom of the list                   */
-/* -------------------------------------------------------------------------- */
-
+const addItemInput = form.querySelector("input") as HTMLInputElement;
+const shopContainer = document.querySelector(".items") as HTMLDivElement;
 const toggleBtn = document.querySelector("#toggle") as HTMLButtonElement;
+const emptyInfo = shopContainer.querySelector("h4") as HTMLHeadingElement;
+const filterInput = document.querySelector("#filter-input") as HTMLInputElement;
+const clearBtn = document.querySelector("#clear") as HTMLButtonElement;
+const modal = document.querySelector("#modal") as HTMLDivElement;
 
-function toggleButton(e: MouseEvent) {
-  console.log("saw");
+const list = createElement("ul") as HTMLUListElement;
+list.setAttribute("id", "items-list");
+list.className = "p-2 grid gap-2 font-bold";
 
-  const isUp = toggleBtn.textContent === "⬆";
-  toggleBtn.textContent = isUp ? "⬇" : "⬆";
-  toggleBtn.setAttribute("data-direction", isUp ? "down" : "up");
-}
+/* ------------------ Add item to top or bottom of the list ----------------- */
 
-toggleBtn.addEventListener("click", toggleButton);
+const toggleButton = () => toggleBtn.classList.toggle("rotate-180");
 
 /* -------------------------- Creating New Element -------------------------- */
 
-function createElement(tag: keyof HTMLElementTagNameMap) {
-  return document.createElement(tag);
+function createElement(tag: keyof HTMLElementTagNameMap, className?: string) {
+  const element = document.createElement(tag);
+  if (tag === "button") element.setAttribute("type", "button");
+  return element;
 }
 
-/* -------------------------------------------------------------------------- */
-/*                           Creating new list item                           */
-/* -------------------------------------------------------------------------- */
-
-function createButtonsOfItem(label: string) {
-  const remBtn = createElement("button") as HTMLButtonElement;
-  remBtn.textContent = label;
-
-  remBtn.addEventListener("click", (e: MouseEvent) => {
-    console.log("clicked on remove button");
-  });
-
-  return remBtn;
-}
+/* ------------------------- Creating new list item ------------------------- */
 
 function createNewItem(value: string) {
-  // li item
+  // <li> item
   const newItem = createElement("li") as HTMLLIElement;
   newItem.setAttribute("id", "item");
   newItem.className = "p-2 flex items-center justify-between border-2 border-black/30 rounded-md";
 
-  // span text
+  // <span> text
   const text = createElement("span");
   text.textContent = value;
 
-  /* --------------------------------- Buttons -------------------------------- */
+  // <div> <buttons> </div>
   const allBtns = createElement("div") as HTMLDivElement;
-
-  allBtns.className = "flex gap-2 bg-gray-300 rounded-md p-1";
-
+  allBtns.className = "flex gap-2 bg-gray-300 rounded-md p-1 items-center";
   const remBtn = createElement("button") as HTMLButtonElement;
   remBtn.textContent = "❌";
-
+  remBtn.setAttribute("id", "remove");
   const editBtn = createElement("button") as HTMLButtonElement;
   editBtn.textContent = "➕";
+  editBtn.setAttribute("id", "edit");
 
-  const doneBtn = createElement("button") as HTMLButtonElement;
-  doneBtn.textContent = "✔";
+  const doneBtn = createElement("input") as HTMLInputElement;
+  doneBtn.type = "checkbox";
+  doneBtn.className = "w-5 h-5";
+  allBtns.appendChild(doneBtn);
 
-  allBtns.addEventListener("click", (e: MouseEvent) => {
-    console.log("clicked on button");
-  });
-
-  [doneBtn, editBtn, remBtn].forEach((btn) => {
-    btn.setAttribute("type", "button");
+  [
+    { id: "edit", el: editBtn },
+    { id: "remove", el: remBtn },
+  ].forEach(({ el: btn, id }) => {
+    btn.setAttribute("id", id);
 
     allBtns.appendChild(btn);
   });
-
-  /* --------------------------------- Buttons -------------------------------- */
 
   newItem.appendChild(text);
   newItem.appendChild(allBtns);
@@ -84,62 +65,203 @@ function createNewItem(value: string) {
   return newItem;
 }
 
-let allValues = localStorage.getItem("list") ? JSON.parse(localStorage.getItem("list")) : ([] as string[]);
+/* ------------------------- Get items from storage ------------------------- */
+function getItemsFromStorage(): string[] {
+  return JSON.parse(localStorage.getItem("items") || "[]");
+}
 
+/* ------------------------------- Remove Item ------------------------------- */
+function removeItem(element: HTMLElement, text: string) {
+  element.remove();
+
+  const filtered = getItemsFromStorage().filter((item) => item !== text);
+
+  if (list.children.length === 0) {
+    replaceListAndEmpty(emptyInfo);
+  }
+  localStorage.setItem("items", JSON.stringify(filtered));
+  updateActions();
+}
+
+/* ------------------------------- Done Item ------------------------------- */
+function checkItem(element: HTMLElement) {
+  console.log(element);
+
+  element.classList.toggle("line-through");
+  element.classList.toggle("font-normal");
+  element.classList.toggle("opacity-50");
+}
+
+function openDialog(element: HTMLElement, type: "remove" | "edit", text: string) {
+  modal.classList.replace("opacity-0", "opacity-100");
+  modal.classList.replace("pointer-events-none", "pointer-events-auto");
+
+  const dialog = document.querySelector("#back-drop") as HTMLDivElement;
+
+  Array.from(dialog.children).forEach((child) => (child.getAttribute("id") !== type ? child.classList.add("hidden") : null));
+
+  console.log(dialog.firstElementChild);
+
+  dialog.addEventListener("click", deleteItem);
+
+  function deleteItem(e: MouseEvent) {
+    const clicked = e.target as HTMLButtonElement;
+
+    const success = clicked.getAttribute("id") === "yes";
+
+    if (clicked.nodeName === "BUTTON" && text) {
+      if (success) {
+        removeItem(element, text);
+      }
+      closeDialog();
+    }
+  }
+}
+
+function closeDialog() {
+  modal.classList.replace("opacity-100", "opacity-0");
+  modal.classList.replace("pointer-events-auto", "pointer-events-none");
+
+  const dialog = document.querySelector("#back-drop") as HTMLDivElement;
+
+  Array.from(dialog.children).forEach((child) => child.classList.add("hidden"));
+}
+
+function addActionstoBtn(e: MouseEvent) {
+  const clickedBtn = e.target as HTMLButtonElement;
+  const parent = clickedBtn.closest("li") as HTMLLIElement;
+  const text = parent.querySelector("span") as HTMLSpanElement;
+
+  switch (clickedBtn.getAttribute("id")) {
+    case "remove":
+      openDialog(parent, "remove", text.textContent!);
+      break;
+    case "edit":
+      // removeItem(parent, text.textContent!);
+      openDialog(parent, "edit", text.textContent!);
+      break;
+    case "done":
+      checkItem(text);
+      break;
+  }
+}
+
+/* ------------------------- Replace Project Content ------------------------ */
+const replaceListAndEmpty = (element: HTMLElement) => shopContainer.replaceChild(element, shopContainer.firstElementChild as Node);
+
+/* ------------------------------- Form Submit ------------------------------ */
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
-
-  const value = addItemInput.value;
+  const value = addItemInput.value.trim();
 
   if (!value) {
     return alert("Please enter a value");
+  } else if (getItemsFromStorage().includes(value)) {
+    return alert("Item already exists");
+  } else {
+    // Add to UI and storage
+    addItemToUI(value);
+    // Add to  storage
+    addItemToStorage(value);
+
+    // Clear and focus input
+    addItemInput.value = "";
+    addItemInput.focus();
+
+    // replaceListAndEmpty
+    updateActions();
   }
+}
 
-  // Select list or create if there is no list yest
-  let list = document.querySelector("#items-list") ?? null;
-
-  if (!list) {
-    list = createElement("ul");
-    list.setAttribute("id", "items-list");
-    list.className = "p-2 grid gap-2 font-bold";
-  }
-
+function addItemToUI(value: string) {
   const item = createNewItem(value);
 
   // add to UI
-  const addToUp = toggleBtn.getAttribute("data-direction") === "up" ? true : false;
-  addToUp ? list.insertAdjacentElement("afterbegin", item) : list.appendChild(item);
-  allValues.push(value);
+  const addToUp = toggleBtn.classList.contains("rotate-180");
 
-  shopList.innerHTML = list.outerHTML;
+  if (addToUp) {
+    list.insertAdjacentElement("afterbegin", item);
+  } else {
+    list.appendChild(item);
+  }
 
-  // add to localstorage
-
-  localStorage.setItem("list", JSON.stringify([...allValues, value]));
-
-  // Clear and focus input
-  addItemInput.value = "";
-  addItemInput.focus();
-
-  // Change opacity ofclear btn
-  clearBtn.classList.remove("disabled");
-  console.log("created");
+  // Replace empty list with new list
+  replaceListAndEmpty(list);
 }
 
-form.addEventListener("submit", handleSubmit);
+function addItemToStorage(value: string) {
+  const items = getItemsFromStorage();
+  items.push(value);
 
-// Clear All Items
-
-// let hasItems = shopList.contains(list);
-
-function clearAll() {
-  const emtpyList = createElement("h4");
-  emtpyList.textContent = "Empty list";
-  emtpyList.className = "text-3xl text-center font-bold my-4 opacity-50";
-
-  // clearBtn.classList.add("disabled");
-
-  shopList.innerHTML = emtpyList.outerHTML;
+  localStorage.setItem("items", JSON.stringify(items));
 }
 
-clearBtn.addEventListener("click", clearAll);
+/* ------------------------------ clearAllItems ----------------------------- */
+
+const clearAll = () => replaceListAndEmpty(emptyInfo);
+
+/* ------------------------------ Filter Items ------------------------------ */
+function filterItems(e: InputEvent) {
+  const allItems = list.querySelectorAll("#item") as NodeListOf<HTMLLIElement>;
+  const value = (e.target as HTMLInputElement).value.toLowerCase();
+
+  allItems.forEach((item) => {
+    const itemContext = item.firstChild?.textContent?.toLowerCase()!;
+
+    if (!itemContext.includes(value)) {
+      item.classList.add("hidden");
+    } else {
+      item.classList.remove("hidden");
+    }
+  });
+
+  e.target as HTMLInputElement;
+}
+
+/* ------------------------------ UI Actions ------------------------------ */
+function emptyListUI() {
+  clearBtn.setAttribute("disabled", "true");
+  filterInput.classList.add("hidden");
+}
+
+function nonEmptyListUI() {
+  console.log("chagirildi");
+
+  filterInput.classList.remove("hidden");
+  clearBtn.removeAttribute("disabled");
+}
+
+function updateActions() {
+  const items = list.querySelectorAll("#item") as NodeListOf<HTMLLIElement>;
+  // it must be reacreated every time it's called because we have to know current state of items
+  if (items.length === 0) emptyListUI();
+  else if (items.length === 1) nonEmptyListUI();
+}
+
+function displayItems() {
+  const storageItems = getItemsFromStorage();
+
+  if (storageItems.length > 0) {
+    storageItems.forEach((item) => {
+      list.appendChild(createNewItem(item));
+    });
+    replaceListAndEmpty(list);
+  }
+}
+
+/* ----------------------------- Event Listeners ---------------------------- */
+
+function initApp() {
+  toggleBtn.addEventListener("click", toggleButton);
+
+  list.addEventListener("click", addActionstoBtn);
+
+  form.addEventListener("submit", handleSubmit);
+  clearBtn.addEventListener("click", clearAll);
+  // filterInput.addEventListener("input", filterItems);
+
+  displayItems();
+  updateActions();
+}
+
+initApp();
